1d091714e1b26719dbb20f8e9471a8d6e7bd0ace
huizhu <zhaohuizhu@ruc.edu.cn>
2017-02-12 22:53:06 +0800

    small change or tied_rank.py

--- utils/tied_rank.py
+++ utils/tied_rank.py
@@ -3,10 +3,9 @@
 # Created Time: 2017年02月10日 星期五 22时24分32秒
 
 import numpy as np
-import auc
 import time
 
-def tied_rank(x):
+def itied_rank(x):
 	sorted_x = sorted(zip(x, range(len(x))))
 	r = [0 for k in x]
 	cur_val = sorted_x[0][0]
@@ -27,48 +26,48 @@ def tied_rank(x):
 			for j in range(last_rank, i+1):
 				r[sorted_x[j][1]] = float(last_rank+i+2)/2.
 			
-def rank(x):
-	def get_ave_rank(start_rank, r):
-		ave_rank = 0.
-		count = 0.
-		for idx in range(start_rank, r):
-			ave_rank += idx
-			count += 1.0	
-		ave_rank /= count
-		return ave_rank
+def tied_rank(x):
+	"""
+	refer https://statistics.laerd.com/statistical-guides/spearmans-rank-order-correlation-statistical-guide.php for details about ranking tied data
 
+	inputs:
+		x: list of num or 1D np.ndarray
+	return:
+		tied_rank of x: list of num
+	"""
 	total_num = len(x)
-	x_sorted = [[ii, r] for ii, r in zip(sorted(x), range(total_num))]
-	print x_sorted
+	x_sorted = [[e, r] for e, r in zip(sorted(x, reverse=True), range(total_num))]
 	
-
-	start_vari = x_sorted[0][0]
+	start_e = x_sorted[0][0]
 	start_rank = 0
-	for v, r in x_sorted:
-		if v == start_vari:
+	for e, r in x_sorted:
+		if e == start_e:
 			pass
 
 		else:
-			ave_rank = get_ave_rank(start_rank, r)
+			mean_rank = np.mean(np.asarray(range(start_rank, r)))
 			for idx in range(start_rank, r):
-				x_sorted[idx][1] = ave_rank
+				x_sorted[idx][1] = mean_rank
 			start_rank = r
-			start_vari = v
+			start_e = e
 		
-	ave_rank = get_ave_rank(start_rank, total_num+1)
+	mean_rank = np.mean(np.asarray(range(start_rank, total_num)))
 	
 	for idx in range(start_rank, total_num):
-		x_sorted[idx][1] = ave_rank
+		x_sorted[idx][1] = mean_rank
 
-	x_rank_dict = dict(x_r for x_r in x_sorted)
+	x_sorted_dict = dict(x_r for x_r in x_sorted)
 
-	return [x_rank_dict[ii]+1 for ii in x]
+	return [x_sorted_dict[e]+1 for e in x]
 
 
 
 if __name__ == '__main__':
 	x = np.arange(10)
-	x = [56, 75, 45, 71, 61, 64, 58, 80, 76, 61]
-	#x = [1, 2, 2, 2]
+	english = [56, 75, 45, 71, 61, 64, 58, 80, 76, 61]
+	math = [66, 70, 40, 60, 65, 56, 59, 77, 67, 63]
 
-	print rank(x)
+	eng_rank = tied_rank(english)
+	math_rank = tied_rank(math)
+	print eng_rank
+	print math_rank
